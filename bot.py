4eeee511406abd29776401cb5bf18e13dba917f8
Aleksey Pribytkov <hamerungg91@gmail.com>
2024-09-27 06:26:04 +0500

    echo bot complete

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
-print('Hello World')
+from bot_token import TOKEN
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def start_command_message(message: Message):
+    await message.answer('Йоу собаки, я Наруто Узумаки!!!')
+
+
+@dp.message(Command(commands=['help']))
+async def help_command_message(message: Message):
+    await message.answer('Да я толком ничего не умею))')
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=f'Вот что ты мне прислал - {message.text}')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+
