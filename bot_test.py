0a85be512b398d99d29937d22f2b3f955fcc8716
Aleksey Pribytkov <hamerungg91@gmail.com>
2024-10-03 04:14:01 +0500

    corrections

--- bot_test.py
+++ bot_test.py
@@ -17,7 +17,7 @@ user = {'in_game': False,
 
 @dp.message(Command(commands='start'))
 async def process_start(message: Message):
-    await message.answer(f'Првиет {message.chat.first_name}\n'
+    await message.answer(f'Привет {message.chat.first_name}\n'
                          f'Я умею играть в Угадай Число !!\n'
                          'что бы получить больше информации\n'
                          'отправь команду - /help')
@@ -26,8 +26,8 @@ async def process_start(message: Message):
 @dp.message(Command(commands='help'))
 async def process_help(message: Message):
     await message.answer(
-        f'Правила игры:\n\nЯ загадаю число от 1 до 100,'
-        f'а тебе надо его угадать\nУ тебя есть {ATTEMPTS} попыток\n'
+        f'Правила игры:\n\nЯ загадаю число от 1 до 100\n'
+        f'А тебе надо его угадать\nУ тебя есть {ATTEMPTS} попыток\n'
         f'Доступные команды:\n\n/cancel - выйти из игры\n'
         f'/stat - посмотреть статистику\nЧтобы начать напиши - давай играть)'
     )
@@ -73,17 +73,16 @@ async def process_num_answer(message: Message):
                 user['wins'] += 1
                 await message.answer('Поздравляю, ты угадал!\nХочешь сыграть ещё?')
             elif player_num > user['secret_num']:
-                await message.answer('Число которое я загадал меньше\n'
-                                     f'У тебя ещё {user['attempts']} попыток')
+                await message.answer('Число которое я загадал меньше')
             else:
-                await message.answer('Число которое я загадал больше'
-                                      f'У тебя ещё {user['attempts']} попыток')
+                await message.answer('Число которое я загадал больше')
         else:
             await message.answer('Число, загаданное мной, находится в диапозоне [1, 100]')
 
         if user['attempts'] == 0:
             user['in_game'] = False
             user['attempts'] = None
+            user['total_played'] += 1
             await message.answer(
                 f'К сожалению у тебя закончились попытки(\n'
                 f'Я загадал число {user['secret_num']}\n'
